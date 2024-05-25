e4ea1a534dc2ded4ba065edb4fe4722175135f9d
snsonsoon <youai058@gmail.com>
2024-05-25 16:59:16 +0900

    Ai model

---
+++ onsets_and_frames/decoding.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+
+
+def extract_notes(onsets, frames, velocity, onset_threshold=0.5, frame_threshold=0.5):
+    onsets = (onsets > onset_threshold).cpu().to(torch.uint8)
+    frames = (frames > frame_threshold).cpu().to(torch.uint8)
+    onset_diff = torch.cat([onsets[:1, :], onsets[1:, :] - onsets[:-1, :]], dim=0) == 1
+
+    pitches = []
+    intervals = []
+    velocities = []
+
+    for nonzero in onset_diff.nonzero():
+        frame = nonzero[0].item()
+        pitch = nonzero[1].item()
+
+        onset = frame
+        offset = frame
+        velocity_samples = []
+
+        while onsets[offset, pitch].item() or frames[offset, pitch].item():
+            if onsets[offset, pitch].item():
+                velocity_samples.append(velocity[offset, pitch].item())
+            offset += 1
+            if offset == onsets.shape[0]:
+                break
+
+        if offset > onset:
+            pitches.append(pitch)
+            intervals.append([onset, offset])
+            velocities.append(np.mean(velocity_samples) if len(velocity_samples) > 0 else 0)
+
+    return np.array(pitches), np.array(intervals), np.array(velocities)
+
+def notes_to_frames(pitches, intervals, shape):
+    roll = np.zeros(tuple(shape))
+    for pitch, (onset, offset) in zip(pitches, intervals):
+        roll[onset:offset, pitch] = 1
+
+    time = np.arange(roll.shape[0])
+    freqs = [roll[t, :].nonzero()[0] for t in time]
+    return time, freqs
